93831b92903ad7fa00f93b4074735c6a3c69576e
caminobelllo <alstpgml0812@gmail.com>
2023-05-25 17:24:13 +0900

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+  const decreaseCount = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Counter>{count}</Counter>
+      <IncreaseBtn onClick={increaseCount}>+</IncreaseBtn>
+      <DecreaseBtn onClick={decreaseCount}>-</DecreaseBtn>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  background-color: #ffe4e1;
+  margin: 30px 40px;
+  display: inline-block;
+  padding: 10px 20px;
+`;
+
+const Counter = styled.h1`
+  margin-left: 42px;
+`;
+
+const IncreaseBtn = styled.button`
+  background-color: pink;
+  border: none;
+  font-size: 30px;
+  padding: 5px 13px 5px 13px;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+  border-radius: 50%;
+  margin-right: 10px;
+`;
+
+const DecreaseBtn = styled.button`
+  background-color: skyblue;
+  border: none;
+  font-size: 30px;
+  padding: 5px 16px 5px 16px;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+  border-radius: 50%;
+`;
